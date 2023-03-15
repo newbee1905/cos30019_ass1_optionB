20e11ec4e2bc2aa770b3009346ec6511c149adae
newbee1905 <beenewminh@outlook.com>
2023-03-15 12:13:16 +1100

    chore: remove trimming string functions

--- include/utils.hh
+++ include/utils.hh
@@ -8,9 +8,12 @@
 #include "scn/detail/fwd.h"
 #include <algorithm>
 #include <array>
+#include <cctype>
+#include <cwchar>
 #include <exception>
 #include <ext/stdio_filebuf.h>
 #include <fstream>
+#include <functional>
 #include <iterator>
 #include <string_view>
 
@@ -102,45 +105,6 @@ FILE *cfile_impl(buffer_t *const fb);
 
 FILE *cfile(std::ofstream const &ofs);
 FILE *cfile(std::ifstream const &ifs);
-
-// trim from start (in place)
-static inline void ltrim(std::string &s) {
-  s.erase(s.begin(),
-          std::find_if(s.begin(), s.end(),
-                       std::not1(std::ptr_fun<int, int>(std::isspace))));
-}
-
-// trim from end (in place)
-static inline void rtrim(std::string &s) {
-  s.erase(std::find_if(s.rbegin(), s.rend(),
-                       std::not1(std::ptr_fun<int, int>(std::isspace)))
-              .base(),
-          s.end());
-}
-
-// trim from both ends (in place)
-static inline void trim(std::string &s) {
-  rtrim(s);
-  ltrim(s);
-}
-
-// trim from start (copying)
-static inline std::string ltrim_copy(std::string s) {
-  ltrim(s);
-  return s;
-}
-
-// trim from end (copying)
-static inline std::string rtrim_copy(std::string s) {
-  rtrim(s);
-  return s;
-}
-
-// trim from both ends (copying)
-static inline std::string trim_copy(std::string s) {
-  trim(s);
-  return s;
-}
 
 }; // namespace kd
 
